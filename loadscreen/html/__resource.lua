5d8458d94629180e786548a7f0d52c8c39726b3b
capu <58012773+capuwu@users.noreply.github.com>
2020-01-24 23:18:00 +0100

    Add files via upload

---
+++ loadscreen/html/__resource.lua
@@ -0,0 +1,13 @@
+files {
+    'index.html',
+    'style.css',
+    'NRPhtml.png',
+    '1.jpg',
+    'script.js',
+    'music.mp3',
+    
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
